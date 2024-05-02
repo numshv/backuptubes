21d8b5b18eb2fce6100f3ed80fa20ba86a014b28
numshv <noumisyifa@gmail.com>
2024-05-02 22:22:02 +0700

    fix - fix pathing tiap module buatan

---
+++ src/account_regist.py
@@ -0,0 +1,52 @@
+import os
+
+#STATING INITIAL STATE OF ALL GLOBAL VARIABLES
+login_state = False
+running_state = True
+
+def csv_parser(line):
+    s=[]
+    j=0
+    for i in range (len(line)):
+        if ','== line [i]:
+            s.append(line[j:i])
+            j=i+1
+    s.append (line[j:])
+    return s
+
+# Fungsi untuk login
+def login():
+    global login_state
+    if login_state == True:
+        print("Anda telah Login dengan username basudara")
+    else:
+        user_path = os.path.join(os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)),'databases', 'user.csv')
+        username = str(input('Masukkan username: '))
+        password = str(input('Masukkan password: '))
+        file = open(user_path, 'r')
+        for line in file:
+            item = csv_parser(line)
+            if username == item[1] and password == item[2]:
+                print('Logged in successfully!')
+                login_state = True
+                return("")
+            elif username == item[1] and password != item[2]:
+                print('passwordnya salah bro, ulang yhh')
+                login()
+        if login_state == False:
+            print("Sorry, you aren't signed up yet. Type 'SIGNUP' to create an account") #masih ke double
+            return("")
+
+# Fungsi untuk sign up
+def sign_up():
+    username = str(input('Username: '))
+    password = str(input('Password: '))
+    user_path = os.path.join(os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)),'databases', 'user.csv')
+    file = open(user_path, 'a')
+    info = '\n' + 'id' + ',' + username + ',' + password + ',' + 'role' + ',' + 'oc'
+    file.write(info)
+
+def logout():
+    global login_state
+    login_state = False
+    print("Anda berhasil logout dari akun basudara")
---
+++ src/tes.py
@@ -0,0 +1,2 @@
+import os
+print(os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)))
