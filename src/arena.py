bdebc0ff279e04347dbaee0ca5f6e3edd70d342a
numshv <19623011@std.stei.itb.ac.id>
2024-05-19 14:50:49 +0700

    battle arena edit

--- src/arena.py
+++ src/arena.py
@@ -130,6 +130,9 @@ Level     : {enemy_level}
 
         sleep(2)
         turn = 1
+        strength_used = False
+        resilience_used = False
+        healing_used = False
         while True:
             damage_diberi = 0
             damage_diterima = 0
@@ -173,39 +176,71 @@ Level       : {enemy_level}
                             break
                         
                         elif player_input == 2:
+                            player_item_inv_arr = []
                             print('\n============ POTION LIST ============')
-                            print_potion(item_inventory_arr, global_id)
-                            print('4. Cancel \n')
+                            print_potion(item_inventory_arr, global_id, player_item_inv_arr)
+                            print(f'{len(player_item_inv_arr)+1}. Cancel \n')
                             
-                            potion_input = int(input('>>> Pilih potion yang ingin digunakan: '))
-                            
-                            if potion_input != 4:
-                                potion_info = item_inventory_arr[potion_input-1]
+                            while True:
+                                potion_input_no = input('>>> Pilih potion yang ingin digunakan: ')
+                                cond_potion_done = False
                                 
-                                if potion_info[1] == 'strength' and potion_info[2] > 0:
-                                    player_mons_info[2] = player_mons_info[2] + (player_mons_info[2] * 0.05)
-                                    print('Setelah meminum potion ini, monster anda merasa semakin kuat!')
-                                
-                                elif potion_info[1] == 'resilience' and potion_info[2] > 0:
-                                    player_mons_info[3] = player_mons_info[3] + (player_mons_info[3] * 0.05)
-                                    print('Setelah meminum potion ini, monster anda merasa lebih kuat!')
-                                
-                                elif potion_info[1] == 'healing' and potion_info[2] > 0:
-                                    player_mons_info[4] = player_mons_info[4] + (base_hp_player * 0.25)
-                                    if player_mons_info[4] > base_hp_player:
-                                        player_mons_info[4] = base_hp_player
-                                    print('Setelah meminum potion ini, monster anda merasa lebih sehat!')
+                                if is_int(potion_input_no) ==True:
+                                    potion_input = int(potion_input_no)
+                                    print(len(player_item_inv_arr))
+                                    if 0<potion_input<len(player_item_inv_arr)+2:
+                                        if 0<potion_input<len(player_item_inv_arr)+1:
+                                            potion_info = player_item_inv_arr[potion_input-1]
+                                            if potion_info[1] == 'strength' and int(potion_info[2]) > 0 and strength_used == False:
+                                                player_mons_info[2] = floor(float(player_mons_info[2]) + (float(player_mons_info[2]) * 0.05))
+                                                potion_info[2] = int(potion_info[2])-1
+                                                strength_used= True
+                                                cond_potion_done= True
+                                                print('Setelah meminum potion ini, monster anda merasa semakin kuat!')
+                                            
+                                            elif potion_info[1] == 'resilience' and int(potion_info[2]) > 0 and resilience_used == False:
+                                                player_mons_info[3] = floor(float(player_mons_info[3]) + (float(player_mons_info[3]) * 0.05))
+                                                potion_info[2] = int(potion_info[2])-1
+                                                resilience_used = True
+                                                cond_potion_done = True
+                                                print('Setelah meminum potion ini, monster anda merasa lebih kuat!')
+                                            
+                                            elif potion_info[1] == 'healing' and int(potion_info[2]) > 0 and healing_used == False:
+                                                healed = floor(float(base_hp_player) * 0.25)
+                                                player_mons_info[4] = int(player_mons_info[4]) + healed
+                                                if player_mons_info[4] > base_hp_player:
+                                                    player_mons_info[4] = base_hp_player
+                                                potion_info[2] = int(potion_info[2])-1
+                                                healing_used = True
+                                                cond_potion_done = True
+                                                print('Setelah meminum potion ini, monster anda merasa lebih sehat!')
+                                        else:
+                                            break
+                                            
+                                        
+                                        if cond_potion_done == True:
+                                            break
+                                        if cond_potion_done == False and 0<potion_input<len(player_item_inv_arr)+1:
+                                            print('Tidak dapat menggunakan potion yang sama lebih dari satu kali')
+                                    
+                                    else:
+                                        print('Input tidak valid')
+                                else:
+                                    print('Input harus berupa integer.')
+                            if cond_potion_done == True:
                                 break
-                                
-                        else:
-                            exit(0)
+                                    
+                        if player_input == 3:
+                            return oc_reward
+                            
+                else:
+                    print('Input harus berupa integer')
             
             sleep(2)
             
             # If menang
             
-            
-            if enemy_info_arr[4] <= 0 and arena_level < 5:
+            if int(enemy_info_arr[4]) <= 0 and arena_level < 5:
                 sleep(2)
                 oc_reward += oc_menang[arena_level]
                 print(f'Selamat, Anda berhasil mengalahkan monster {enemy_info_arr[1]} !!!')
@@ -213,7 +248,7 @@ Level       : {enemy_level}
                 arena_level += 1
                 break
             
-            elif enemy_info_arr[4] <= 0 and arena_level == 5:
+            elif int(enemy_info_arr[4]) <= 0 and arena_level == 5:
                 sleep(2)
                 oc_reward += oc_menang[arena_level]
                 print(f'Selamat, Anda berhasil mengalahkan monster {enemy_info_arr[1]} !!!')
@@ -232,10 +267,11 @@ Level       : {enemy_level}
             
             #Bagian enemy attack
             
-            enemy_attack = int(enemy_info_arr[2]) + (int(enemy_info_arr[2]) * 0.01 * RNG(-30, 30))
-            damage_diterima_cur = (enemy_attack * (100 - player_mons_info[3])*0.01)
-            damage_diterima += damage_diterima_cur
-            player_mons_info[4] = floor(int(player_mons_info[4]) - (damage_diterima_cur))
+            enemy_attack = float(enemy_info_arr[2]) + (float(enemy_info_arr[2]) * 0.01 * RNG(-30, 30))
+            pengali = (1 - (int(player_mons_info[3])*0.01))
+            deal_damage = floor(enemy_attack * pengali)
+            damage_diterima += deal_damage
+            player_mons_info[4] = int(player_mons_info[4]) - deal_damage
             
             sleep(2)
             print(f"""
@@ -295,12 +331,14 @@ def arena(monster_inventory_arr, global_id, monster_arr, item_inventory_arr):
         
         sleep(1)
         print("""
-    _____                                      __________                      ._. 
-    /  _  \ _______   ____    ____  _____       \____    / ____    ____    ____ | | 
-    /  /_\  \\_  __ \_/ __ \  /    \ \__  \        /     / /  _ \  /    \ _/ __ \| | 
-    /    |    \|  | \/\  ___/ |   |  \ / __ \_     /     /_(  <_> )|   |  \\  ___/ \| 
-    \____|__  /|__|    \___  >|___|  /(____  /    /_______ \\____/ |___|  / \___  >__ 
-            \/             \/      \/      \/             \/            \/      \/ \/ """)
+   _____                                      __________                      ._. 
+  /  _  \ _______   ____    ____  _____       \____    / ____    ____    ____ | | 
+ /  /_\  \\_  __ \_/ __ \  /    \ \__  \        /     / /  _ \  /    \ _/ __ \| | 
+/    |    \|  | \/\  ___/ |   |  \ / __ \_     /     /_(  <_> )|   |  \\  ___/ \| 
+\____|__  /|__|    \___  >|___|  /(____  /    /_______ \\____/ |___|  / \___  >__ 
+        \/             \/      \/      \/             \/            \/      \/ \/ 
+                                                                                  
+""")
         
         sleep(1.5)
         
@@ -310,6 +348,8 @@ def arena(monster_inventory_arr, global_id, monster_arr, item_inventory_arr):
         monster_id = [0]
         monster_level = [0]
 
+        actual_i = 0
+        
         for i in range (len(monster_inventory_arr)):
             if monster_inventory_arr[i][0] == global_id:
                 for j in range (len(monster_arr)):
@@ -317,22 +357,30 @@ def arena(monster_inventory_arr, global_id, monster_arr, item_inventory_arr):
                         cur_monster = monster_arr[j][1]
                         monster_name.append(monster_arr[j][1])
                         monster_id.append(monster_arr[j][0])
-                print(f"{i+1}. {cur_monster} (Level: {monster_inventory_arr[i][2]})")
+                        actual_i += 1
+                print(f"{actual_i}. {cur_monster} (Level: {monster_inventory_arr[i][2]})")
                 monster_level.append(int(monster_inventory_arr[i][2]))
         
 
         player_mons_lvl = 0
         select_number = -999
         
-        while True:
-            select_number = int(input('\n>>> Pilih monster nomor: '))
-            if select_number <= len(monster_name):
-                player_mons_lvl = monster_level[select_number]
-                break
-            else:
-                print('Input tidak valid!')
+        print('\nPilih monster yang akan kamu ajak bertarung!')
         
-        player_mons_info_no = monster_arr[select_number-1]
+        while True:
+            select_number_no = input('\n>>> Pilih monster nomor: ')
+            if is_int(select_number_no) == True:
+                select_number= int(select_number_no)
+                if select_number <= len(monster_name):
+                    player_mons_lvl = monster_level[select_number]
+                    break
+                else:
+                    print('Input tidak valid!')
+            else:
+                print('Input harus berupa integer.')
+        
+        selected_id = monster_id[select_number]
+        player_mons_info_no = monster_arr[int(selected_id)-1]
         
         # Copy
         player_mons_info = player_mons_info_no.copy()
@@ -342,27 +390,16 @@ def arena(monster_inventory_arr, global_id, monster_arr, item_inventory_arr):
             for i in range(2, 5):
                 player_mons_info[i] = int(float(player_mons_info[i]) + float(player_mons_info[i]) * float((player_mons_lvl-1)) * 0.1)
         
+        print(mons_pict[RNG(0,4)])
         print(f"""
+              
+RAWRRR, Agent X mengeluarkan monster {player_mons_info[1]} !!!
 
-            /\----/\_   
-            /         \   /|
-            |  | O    O | / |
-            |  | .vvvvv.|/  /
-        /   | |     |   /
-        /    | `^^^^^   /
-        | /|  |         /
-        / |    ___    |
-            \  |   |   |
-            |  |   |   |
-            \._\   \._\ 
-
-    RAWRRR, Agent X mengeluarkan monster {player_mons_info[1]} !!!
-
-    Name      : {player_mons_info[1]}
-    ATK Power : {player_mons_info[2]}
-    DEF Power : {player_mons_info[3]}
-    HP        : {player_mons_info[4]}
-    Level     : {player_mons_lvl}
+Name      : {player_mons_info[1]}
+ATK Power : {player_mons_info[2]}
+DEF Power : {player_mons_info[3]}
+HP        : {player_mons_info[4]}
+Level     : {player_mons_lvl}
     """)
 
         sleep(2)
